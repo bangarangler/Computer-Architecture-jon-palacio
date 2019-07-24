bc0524c0eb98d816e4a63e007fd840b934ce0517
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-24 18:51:44 -0400

    MVP Complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,12 +1,18 @@
 """CPU functionality."""
+
 import sys
+
 class CPU:
     """Main CPU class."""
+
     def __init__(self):
         """Construct a new CPU."""
         self.register = [0] * 8
         self.ram = [0] * 256
+        self.register[7] = len(self.ram) - 1    # stack pointer
+        # self.register[8] = 0                    # program pointer
         self.pc = 0
+        # *************** Dispatcher table ****************** #
         self.branchtable = {}
         self.branchtable[int(0b10100000)] = self.handle_ADD
         self.branchtable[int(0b10100011)] = self.handle_SUB
@@ -15,7 +21,11 @@ class CPU:
         # self.branchtable[int(0b01001000)] = self.handle_PRA
         self.branchtable[int(0b01000111)] = self.handle_PRN
         self.branchtable[int(0b10000010)] = self.handle_LDI
-        # self.branchtable[int(0b00000001)] = self.handle_HLT
+        self.branchtable[int(0b01000110)] = self.pop_it_like_its_hot
+        self.branchtable[int(0b01000101)] = self.push_it_real_good
+
+    # ***************** Dispatcher functions ****************** #
+    # ******** ALU functions ****** #
     def handle_ADD(self, operand_a, operand_b):     #  ADD
         self.alu("ADD", operand_a, operand_b)
         self.pc += 3
@@ -28,43 +38,78 @@ class CPU:
     def handle_DIV(self, operand_a, operand_b):     # Divide
         self.alu("DIV", operand_a, operand_b)
         self.pc += 3
+
     # def handle_PRA(self, operand_a, operand_b):     #
+
     def handle_PRN(self, operand_a, operand_b):     # Print number in register
         print(f'{self.register[operand_a]}')
         self.pc += 2
     def handle_LDI(self, operand_a, operand_b):     # LDI:
         self.register[operand_a] = operand_b
         self.pc += 3
+
+    # ******** Stack functions ****** #
+    def push_it_real_good(self, operand_a, operand_b):      # PUSH to RAM
+        # print("SUDO PUSH")
+        # print("Reg 7: ", self.register[7])
+        self.ram[self.register[7]] = self.register[operand_a]
+        self.register[7] -= 1 # self.ram[254]
+        self.pc += 2
+
+    def pop_it_like_its_hot(self, operand_a, operand_b):       # POP from RAM
+        # print("SUDO POP")
+        self.register[7] += 1
+        self.register[operand_a] = self.ram[self.register[7]]
+        self.pc += 2
+
+    # ***************** Dispatcher Function ****************** #
+
     def dispatch(self, IR, opA, opB):
         self.branchtable[IR](opA, opB)
+
+
+
+    # ***************** Load program ****************** #
     def load(self):
         """Load a program into memory."""
+
         if len(sys.argv) is not 2:
             print(f"usage: {sys.argv[0]} <filename>")
             sys.exit(1)
+
         try:
             address = 0
             program_name = sys.argv[1]
+
             with open(program_name) as f:
                 for line in f:
                     num = line.split("#", 1)[0]
+
                     if num.strip() == '':  # ignore comment-only lines
                         continue
                     num = '0b' + num
                     # print(num)
                     self.ram[address] = int(num, 2)
+                    # self.register[8] += 1
                     address += 1
+
             print(self.ram)
+
         except FileNotFoundError:
             print(f"{sys.argv[0]}: {sys.argv[1]} not found")
             sys.exit(2)
+
+
     def ram_read(self, address):
         return self.ram[address]
+
     def ram_write(self, value, address):
         self.ram[address] = value
         pass
+
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
+
         if op == "ADD":
             self.register[reg_a] += self.register[reg_b]
         elif op == "SUB":
@@ -75,11 +120,13 @@ class CPU:
             self.register[reg_a] /= self.register[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+
     def trace(self):
-        """
+        """            IR = self.pc
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
+
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
             #self.fl,
@@ -88,19 +135,29 @@ class CPU:
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
+
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
+        # self.register[7] = len(self.ram) - 1    # stack pointer
+        # self.register[8] = 0                    # program pointer
         print()
+
     def run(self):
         running = True
         while running:
+
             IR = self.pc
             operand_a = self.ram_read(IR + 1)
             operand_b = self.ram_read(IR + 2)
-            # print("IR: ", IR)
+            # self.trace()
+            # print("I0R: ", IR)
             if self.ram[IR] == int(0b00000001):               # HLT base case: exit loop
                 print("HALT")
                 running = False
             else:
-                # print(self.ram[IR])
+                print(self.ram[IR])
+                print(operand_a)
+                print(operand_b)
+                print()
                 self.dispatch(self.ram[IR], operand_a, operand_b)
+
